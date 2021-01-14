8e14c2d9220673d8fce279d1624c373fc3199244
Hari Ram J P <hariramjp777@gmail.com>
2021-01-14 10:19:30 +0530

    Add checkbox feature
    Checkbox style, line-through when complete

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,26 @@
+const add = document.getElementById("add-btn");
+const txtInput = document.querySelector(".txt-input");
+const checkboxes = document.querySelectorAll(".cb-input");
+checkboxes.forEach(function (checkbox) {
+  checkbox.addEventListener("input", function () {
+    complete(this);
+  });
+});
+add.addEventListener("click", addTodo);
 
+function addTodo() {
+  const item = txtInput.value.trim();
+  if (item) {
+    console.log(item);
+    txtInput.value = "";
+    txtInput.focus();
+  }
+}
+
+function complete(checkbox) {
+  if (checkbox.checked) {
+    checkbox.parentElement.classList.add("checked");
+  } else {
+    checkbox.parentElement.classList.remove("checked");
+  }
+}
