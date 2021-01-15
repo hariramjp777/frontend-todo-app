b2f31101345b52f75f5225b80171d99285f007c7
Hari Ram J P <hariramjp777@gmail.com>
2021-01-15 20:05:44 +0530

    add light theme feature

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,20 @@ document.addEventListener("DOMContentLoaded", main);
 /* MAIN */
 
 function main() {
+  /* Theme switcher */
+  document
+    .getElementById("theme-switcher")
+    .addEventListener("click", function () {
+      document.querySelector("body").classList.toggle("light");
+      const themeImg = this.children[0];
+      themeImg.setAttribute(
+        "src",
+        themeImg.getAttribute("src") === "./assets/images/icon-sun.svg"
+          ? "./assets/images/icon-moon.svg"
+          : "./assets/images/icon-sun.svg"
+      );
+    });
+
   /* GET ALL TODOs */
   addTodo();
 
